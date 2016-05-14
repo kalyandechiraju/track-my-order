dde4dddf49c9035983f65c39b4da4115c03891b7
chandan coolz <chandankumarsingh789@gmail.com>
2016-05-14 14:39:19 +0530

    added login and main activity

---
+++ app/src/main/java/com/challengers/trackmyorder/LoginDBoy.java
@@ -0,0 +1,14 @@
+package com.challengers.trackmyorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginDBoy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_dboy);
+        setTitle("Login");
+    }
+}
